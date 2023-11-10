551afad63b0de332e23cd8f04379f5e26f85ce3e
Meet Suthar <meetsuthar299@gmail.com>
2023-11-09 21:26:26 -0700

    improved styles, added a scroll down icon.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,51 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Skills from "../components/Skills";
+import Skills from '../components/Skills';
 import Education from '../components/Education';
 import Experience from '../components/Experience';
-import { useResumeDataContext } from "../Providers/ResumeDataProvider";
-import UserImg from "../Assets/images/meetPic.jpg";
+import { useResumeDataContext } from '../Providers/ResumeDataProvider';
+import UserImg from '../Assets/images/meetPic.jpg';
 import '../styles/style.css';
+import { FaChevronDown } from 'react-icons/fa';
+import { Link } from 'react-scroll';
+
 
 const About = () => {
     const { resumeData } = useResumeDataContext();
-
     const aboutInfoTitleRef = useRef(null);
-    const aboutTabsRef = useRef(null);
-
-    useEffect(() => {
-        const observerOptions = {
-            root: null,
-            rootMargin: "0px",
-            threshold: 0.5,
-        };
-
-        const aboutInfoTitleObserver = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    aboutInfoTitleRef.current.classList.add("opacity-100", "translate-y-0");
-                }
-            });
-        }, observerOptions);
-
-        const aboutTabsObserver = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    aboutTabsRef.current.classList.add("opacity-100", "translate-y-0");
-                }
-            });
-        }, observerOptions);
-
-        aboutInfoTitleObserver.observe(aboutInfoTitleRef.current);
-        aboutTabsObserver.observe(aboutTabsRef.current);
-
-        return () => {
-            aboutInfoTitleObserver.disconnect();
-            aboutTabsObserver.disconnect();
-        };
-    }, []);
-
     const [marginTop, setMarginTop] = useState(0);
-    useEffect(() => {
+    const [activeTab, setActiveTab] = useState('');
+
+    useEffect(() => {   
         const handleResize = () => {
             const screenHeight = window.innerHeight;
             setMarginTop(screenHeight);
@@ -56,8 +26,6 @@ const About = () => {
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, []);
-
-    const [activeTab, setActiveTab] = useState('');
 
     const handleTabClick = (tab) => {
         setActiveTab(tab);
@@ -73,16 +41,23 @@ const About = () => {
     }
 
     return (
-        <div id="about" className="relative mx-auto threeDWrap sm:max-w-2xl md:max-w-full lg:max-w-screen md:px-26 lg:px-8 mt-screen z-10 bg-[rgba(46,46,46,0.69)] backdrop-blur border-black rounded-t-3xl text-white" style={{ marginTop }}>
-            <div className="flex flex-col lg:flex-row p-10">
-                <div className='flex justify-center px-10 h-96'>
-                    <img src={UserImg} alt="Meet Suthar" className="object-cover w-full shadow-2xl transition-all rounded-xl hover:rounded-xl duration-700 hover:scale-105 mr-auto" />
-                </div>
+        <div id="about" className="relative mx-auto threeDWrap sm:max-w-2xl md:max-w-full lg:max-w-screen md:px-8 lg:px-12 mt-screen z-10 bg-[rgba(46,46,46,0.69)] backdrop-blur border-black rounded-t-3xl text-white" style={{ marginTop }}>
+            <div id="scrollIcon" className="fixed pb-24 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center text-white">
+                <Link
+                    to={"about"}
+                    smooth={true}
+                    duration={1000}
+                    offset={-100}
+                >
+                    <FaChevronDown className="text-3xl animate-bounce" />
+                </Link>
+            </div>
+            <div className="flex flex-col lg:flex-row p-1 lg:p-12">
 
-                <div id="aboutInfo" className="about-info rounded-3xl shadow-xl mt-8 lg:mt-0 lg:ml-8 p-8 max-w-5xl backdrop-blur bg-[rgba(0,0,0,0.53)] ">
+                <div id="aboutInfo" className="lg:w-1/2 lg:mr-8 lg:ml-4 p-8 max-w-5xl backdrop-blur bg-[rgba(0,0,0,0.53)] rounded-xl">
                     <div
                         id="aboutInfoTitle"
-                        className="opacity-0 transform transition-opacity duration-500 delay-500 ease-in-out"
+                        className="opacity-1 transform transition-opacity duration-500 ease-in-out"
                         ref={aboutInfoTitleRef}
                     >
                         <h1 className="text-4xl pb-4">About Me</h1>
@@ -94,37 +69,29 @@ const About = () => {
                     <hr className="w-full my-6 border-gray-300" />
                     <div>
                         <nav id="AboutTabTitles" className="flex justify-center mt-4 text-center">
-                            <button
-                                className={`text-lg text-white transition-all duration-200 ease-in-out hover:scale-110 ${activeTab === 'skills' ? 'bg-[rgba(255,255,255,0.21)]' : ''
-                                    } pr-3 pl-3 mr-1 ml-1 rounded-xl focus:bg-[rgba(139,139,139,0.21)] hover:shadow-2xl w-28 pt-2`}
-                                onClick={() => handleTabClick('skills')}
-                            >
-                                Skills
-                            </button>
-                            <button
-                                className={`text-lg text-white transition-all duration-200 ease-in-out hover:scale-110 ${activeTab === 'experience' ? 'bg-[rgba(255,255,255,0.21)]' : ''
-                                    } pr-3 pl-3 mr-1 ml-1 rounded-xl focus:bg-[rgba(139,139,139,0.21)] hover:shadow-2xl w-28 pt-2`}
-                                onClick={() => handleTabClick('experience')}
-                            >
-                                Experience
-                            </button>
-                            <button
-                                className={`text-lg text-white transition-all duration-200 ease-in-out hover:scale-110 ${activeTab === 'education' ? 'bg-[rgba(255,255,255,0.21)]' : ''
-                                    } pr-3 pl-3 mr-1 ml-1 rounded-xl focus:bg-[rgba(139,139,139,0.21)] hover:shadow-2xl w-28 h-12 pt-2`}
-                                onClick={() => handleTabClick('education')}
-                            >
-                                Education
-                            </button>
+                            {['skills', 'experience', 'education'].map((tab) => (
+                                <button
+                                    key={tab}
+                                    className={`text-lg text-white transition-all duration-200 ease-in-out hover:scale-110 ${activeTab === tab ? 'bg-[rgba(255,255,255,0.21)]' : ''
+                                        } pr-3 pl-3 mr-1 ml-1 rounded-xl focus:bg-[rgba(139,139,139,0.21)] hover:shadow-2xl w-28 pt-2`}
+                                    onClick={() => handleTabClick(tab)}
+                                >
+                                    {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                                </button>
+                            ))}
                         </nav>
                     </div>
                     <div
                         id="AboutTabs"
-                        className="mt-8 opacity-0 transform transition-opacity duration-500 delay-500 ease-in-out"
-                        ref={aboutTabsRef}
+                        className="mt-8 opacity-1 transform transition-opacity duration-500 ease-in-out"
                     >
                         {tabContent}
                     </div>
                 </div>
+                <div className="lg:w-1/2">
+                    <img src={UserImg} alt="Meet Suthar" className="object-cover w-full h-96 lg:h-auto lg:w-full shadow-2xl transition-all rounded-xl" />
+                </div>
+
             </div>
         </div>
     );
