679a47acea654f6a6ec30fdf75a2a99d65514155
Meet Suthar <meetsuthar299@gmail.com>
2023-12-10 00:36:42 -0700

    cleaned up code and added new project.

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,67 +1,63 @@
 import React, { useState } from "react";
-import { FaImages, FaReact, FaReacteurope, FaStripe, FaGithub, FaExternalLinkAlt, FaFigma } from 'react-icons/fa';
-import { SiFirebase, SiRedux } from 'react-icons/si';
+import { FaImages, FaReact, FaStripe, FaGithub, FaExternalLinkAlt, FaFigma, FaNode } from 'react-icons/fa';
+import { RiJavascriptLine } from "react-icons/ri";
+import { SiFirebase, SiRedux, SiTailwindcss } from 'react-icons/si';
 import netflixClone from '../Assets/images/NetflixClone.jpg';
 import homeWard from '../Assets/images/Homeward.jpg';
 import rockPaperScissors from '../Assets/images/rockPaperSc.jpg';
+import shopMIT from "../Assets/images/shopMIT.png"
 
 const projectsData = [
     {
         id: 1,
-        title: 'Netflix Clone',
-        description: 'The Netflix clone is a web application built with ReactJS, featuring Firebase authentication, Firestore integration for storing user preferences, Redux for state management, and Stripe for simulated payment processing. It uses the TMDB API to provide a dynamic selection of movies, offering a realistic user experience similar to the actual Netflix platform.',
-        image: netflixClone,
-        technologies: ['JavaScript', 'ReactJS', 'Redux', 'Firebase', 'Stripe'],
-        github:"https://github.com/meetSuthar299/netflix-build",
-        projectLink:"https://meetsuthar299.github.io/netflix-build/#/",
+        title: 'ShopMIT E-Commerce App',
+        description: 'A full stack ecommerce website built using the MERN stack for MIT Infosys for customer facing store to order products for local delivery, and internal  inventory management/tracking. (under development)',
+        image: shopMIT,
+        technologies: ['JavaScript', 'ReactJS', "NodeJS", 'Redux', 'Mongo DB', 'Express'],
+        projectLink: "https://shopmit.vercel.app/",
     },
     {
         id: 2,
-        title: 'Homeward-App',
-        description: 'Homeward is a React Native expo-managed mobile application for finding lost pets. Users may create posts to spread the word and get help from the community in reuniting with their pets.',
-        image: homeWard,
-        technologies: ['JavaScript', 'React Native', 'Firebase'],
-        github:"https://github.com/meetSuthar299/homeward-App",
-        figma:"https://www.figma.com/file/ngq0iER7f3X3sTdhsYz2IK/Homeward-Main"
+        title: 'Netflix Clone',
+        description: 'The Netflix clone is a web application built with ReactJS, featuring Firebase authentication, Firestore integration for storing user preferences, Redux for state management, and Stripe for simulated payment processing. It uses the TMDB API to provide a dynamic selection of movies and related videos.',
+        image: netflixClone,
+        technologies: ['JavaScript', 'ReactJS', 'Redux', 'Firebase', 'Stripe'],
+        github: "https://github.com/meetSuthar299/netflix-build",
+        projectLink: "https://meetsuthar299.github.io/netflix-build/#/",
     },
     {
         id: 3,
+        title: 'Homeward-App',
+        description: 'Homeward is a React Native expo-managed mobile application for finding lost pets. Users may create posts to spread the word and get help from the community in reuniting with their pets.',
+        image: homeWard,
+        technologies: ['JavaScript', 'React Native', 'Firebase', "NodeJS"],
+        github: "https://github.com/meetSuthar299/homeward-App",
+        figma: "https://www.figma.com/file/ngq0iER7f3X3sTdhsYz2IK/Homeward-Main"
+    },
+    {
+        id: 4,
         title: 'Rock Paper Scissors Game',
         description: 'A simple Rock Paper Scissors game made using ReactJS and tailwindCSS. The game can be played against the computer, or have the computers battle themselves.',
         image: rockPaperScissors,
-        technologies: ['JavaScript', 'ReactJS', "tailwindCSS"],
-        projectLink:"https://meetsuthar299.github.io/Rock-Paper-Scissors/",
-        github:"https://github.com/meetSuthar299/Rock-Paper-Scissors"
+        technologies: ['JavaScript', 'ReactJS', "TailwindCSS"],
+        projectLink: "https://meetsuthar299.github.io/Rock-Paper-Scissors/",
+        github: "https://github.com/meetSuthar299/Rock-Paper-Scissors"
     },
 ];
 
-const TechIcon = ({ tech }) => {
-    switch (tech) {
-        case 'JavaScript':
-            return <FaReact />;
-        case 'ReactJS':
-            return <FaReact />;
-        case 'React Native':
-            return <FaReacteurope />;
-        case 'TailwindCSS':
-            return <FaReact />;
-        case 'Stripe':
-            return <FaStripe />;
-        case 'Firebase':
-            return <SiFirebase />;
-        case 'Redux':
-            return <SiRedux />;
-        default:
-            return null;
-    }
-};
-
+const LinkIcon = ({ href, icon, tooltip }) => (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+        {icon &&
+            React.cloneElement(icon, {
+                className:
+                    "h-6 w-6 text-gray-400 hover:text-gray-500 transition-all duration-200 hover:scale-110",
+                title: tooltip,
+            })}
+    </a>
+);
 
 const ProjectCard = ({ project }) => (
-    <div
-        className="max-w-md mx-auto overflow-hidden backdrop-filter backdrop-blur-lg text-center text-white hover:shadow-lg  bg-[rgba(0,0,0,0.32)] border-black rounded-xl shadow-md animate-fade-in"
-        //onClick={() => window.open(project.github)}
-    >
+    <div className="max-w-md mx-auto overflow-hidden backdrop-filter backdrop-blur-lg text-center text-white hover:shadow-lg bg-[rgba(0,0,0,0.32)] border-black rounded-xl shadow-md animate-fade-in">
         <img
             className="w-full h-48 object-cover rounded-t-lg"
             src={project.image}
@@ -90,10 +86,18 @@ const ProjectCard = ({ project }) => (
             <p className="text-sm">{project.description}</p>
             <div className="mt-4 space-x-2 flex justify-center">
                 {project.technologies.map((tech, index) => (
-                    <TechIcon key={index} tech={tech} alt={tech} />
+                    <div key={index} className="relative inline-block" title={tech}>
+                        {tech === "JavaScript" && <RiJavascriptLine />}
+                        {tech === "ReactJS" && <FaReact />}
+                        {tech === "React Native" && <FaReact />}
+                        {tech === "TailwindCSS" && <SiTailwindcss />}
+                        {tech === "Stripe" && <FaStripe />}
+                        {tech === "Firebase" && <SiFirebase />}
+                        {tech === "Redux" && <SiRedux />}
+                        {tech === "NodeJS" && <FaNode />}
+                    </div>
                 ))}
             </div>
-            
         </div>
     </div>
 );
@@ -108,32 +112,33 @@ const ProjectList = ({ projects }) => (
 
 const ProjectFilter = ({ technologies, onFilterChange }) => (
     <div className="flex flex-wrap justify-center gap-2 mt-8 p-4">
-      {technologies.map((tech) => (
-        <button
-          key={tech}
-          onClick={() => onFilterChange(tech)}
-          className="text-lg text-white transition-all duration-200 ease-in-out hover:scale-110 bg-[rgba(42,42,42,0.16)] px-4 py-2 m-1 rounded-xl focus:bg-[rgba(139,139,139,0.21)] hover:shadow-2xl"
-        >
-          {tech}
-        </button>
-      ))}
+        {technologies.map((tech) => (
+            <button
+                key={tech}
+                onClick={() => onFilterChange(tech)}
+                className="text-lg text-white transition-all duration-200 ease-in-out hover:scale-110 bg-[rgba(42,42,42,0.16)] px-4 py-2 m-1 rounded-xl focus:bg-[rgba(139,139,139,0.21)] hover:shadow-2xl"
+            >
+                {tech}
+            </button>
+        ))}
     </div>
-  );
+);
+
 export default function Projects() {
     const [filteredProjects, setFilteredProjects] = useState(projectsData);
-
-    // Extracting unique technologies from all projects
-    const allTechnologies = Array.from(new Set(projectsData.flatMap((project) => project.technologies)));
-
-    const [technologies, setTechnologies] = useState(['All', ...allTechnologies]);
+    const allTechnologies = Array.from(
+        new Set(projectsData.flatMap((project) => project.technologies))
+    );
+    const [technologies] = useState(["All", ...allTechnologies]);
 
     const handleFilterChange = (tech) => {
-        if (tech === 'All') {
-            setFilteredProjects(projectsData);
-        } else {
-            const filtered = projectsData.filter((project) => project.technologies.includes(tech));
-            setFilteredProjects(filtered);
-        }
+        setFilteredProjects(
+            tech === "All"
+                ? projectsData
+                : projectsData.filter((project) =>
+                    project.technologies.includes(tech)
+                )
+        );
     };
 
     return (
@@ -143,12 +148,15 @@ export default function Projects() {
                     <h2 className="max-w-lg mb-6 font-sans text-5xl font-bold text-white sm:text-4xl">
                         <span className="relative inline-block text-3xl">
                             <FaImages />
-                        </span>{' '}
+                        </span>{" "}
                         Projects
                     </h2>
                 </div>
                 <div className="container mx-auto">
-                    <ProjectFilter technologies={technologies} onFilterChange={handleFilterChange} />
+                    <ProjectFilter
+                        technologies={technologies}
+                        onFilterChange={handleFilterChange}
+                    />
                     <ProjectList projects={filteredProjects} />
                 </div>
             </div>
